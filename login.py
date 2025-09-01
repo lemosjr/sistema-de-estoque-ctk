5d421676380a7513214627fd6d5b5398430c1e8e
lemosjr <160804009+lemosjr@users.noreply.github.com>
2025-09-01 20:15:02 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,14 +1,13 @@
 import customtkinter as ctk
 from tkinter import messagebox
 import json
-import os  # Importe a biblioteca 'os'
+import os
 
 def open_arquivo():
     if os.path.exists("usuarios.json"):
         with open("usuarios.json", "r") as file:
             return json.load(file)
     else:
-        # Cria o arquivo com um dicionário vazio se ele não existir
         with open("usuarios.json", "w") as file:
             json.dump({}, file)
             return {}
@@ -98,21 +97,25 @@ def abrir_cadastro():
     frame_principal = ctk.CTkFrame(root)
     frame_principal.pack(padx=30, pady=30, fill="both", expand=True)
 
+    # Nome Completo
     label_nome = ctk.CTkLabel(frame_principal, text="Nome Completo:", font=("Arial", 12))
     label_nome.grid(row=0, column=0, pady=10, sticky="w")
     entry_nome = ctk.CTkEntry(frame_principal, placeholder_text="Digite seu nome completo", font=("Arial", 12))
     entry_nome.grid(row=0, column=1, pady=10, padx=10, sticky="ew")
 
+    # Email
     label_email = ctk.CTkLabel(frame_principal, text="Email:", font=("Arial", 12))
     label_email.grid(row=1, column=0, pady=10, sticky="w")
     entry_email = ctk.CTkEntry(frame_principal, placeholder_text="Digite seu email", font=("Arial", 12))
     entry_email.grid(row=1, column=1, pady=10, padx=10, sticky="ew")
 
+    # Usuário
     label_usuario = ctk.CTkLabel(frame_principal, text="Usuário:", font=("Arial", 12))
     label_usuario.grid(row=2, column=0, pady=10, sticky="w")
     entry_usuario = ctk.CTkEntry(frame_principal, placeholder_text="Digite seu nome de usuário", font=("Arial", 12))
     entry_usuario.grid(row=2, column=1, pady=10, padx=10, sticky="ew")
 
+    # Senha
     label_senha = ctk.CTkLabel(frame_principal, text="Senha:", font=("Arial", 12))
     label_senha.grid(row=3, column=0, pady=10, sticky="w")
     entry_senha = ctk.CTkEntry(frame_principal, placeholder_text="Digite sua senha", show="*", font=("Arial", 12))
@@ -120,10 +123,11 @@ def abrir_cadastro():
 
     frame_botoes = ctk.CTkFrame(frame_principal)
     frame_botoes.grid(row=4, column=0, columnspan=2, pady=20)
-
+    # Botão cadastrar
     bnt_cadastrar = ctk.CTkButton(frame_botoes, text="Cadastrar", font=("Arial", 12), command=bnt_cadastrar, width=20)
     bnt_cadastrar.grid(row=0, column=0, padx=20)
-
+    
+    # Botão cancelar
     bnt_cancelar = ctk.CTkButton(frame_botoes, text="Cancelar", font=("Arial", 12), fg_color="#a83232", command=bnt_cancelar, hover_color="#F24B88", width=20)
     bnt_cancelar.grid(row=0, column=1, padx=20)
 
